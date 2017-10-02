2d79feb400643f2c09d52e0b0ad6566abf06cd61
Dmitry Ryzhkov <dmitry@microsoft.com>
2017-10-01 21:47:44 -0700

    Added material-ui

--- src/components/SkillzPage.jsx
+++ src/components/SkillzPage.jsx
@@ -1,42 +1,55 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
 
 export default class SkillzPage extends React.Component {
   render() {
+    const style = {
+      height: 100,
+      width: 100,
+      textAlign: 'center',
+      display: 'inline-block',
+      backgroundColor: '#fffeee',
+    };
+
+    const programming = {
+      title: 'Development',
+      items: ['JavaScript', 'NodeJS', 'Angular', 'React', 'C#', 'Java', 'GoLang', 'PHP', 'C++']
+    };
+
+    const tools = {
+      title: 'IDEs / Tools',
+      items: ['Bash', 'VSCode', 'Visual Studio', 'Android Studio', 'Git', 'Grunt', 'Gulp', 'Webpack']
+    };
+
+    const systems = {
+      title: 'Operating Systems',
+      items: ['Linux', 'MacOS', 'Windows']
+    };
+
+    const skillz = [programming, tools, systems];
+
     return (
       <div id="skillz" className={'container-fluid'}>
-      <h1>Skills</h1>
-      <div style={{ overflowX: 'auto' }}>
-        <h2>Development</h2>
-        <ul>
-          <li>JavaScript</li>
-          <li>NodeJS</li>
-          <li>Angular</li>
-          <li>React</li>
-          <li>C#</li>
-          <li>Java</li>
-          <li>GoLang</li>
-          <li>PHP</li>
-          <li>C++</li>
-        </ul>
-        <h2>IDE / Tools</h2>
-        <ul>
-          <li>Bash</li>
-          <li>VSCode</li>
-          <li>Visual Studio</li>
-          <li>Android Studio</li>
-          <li>Git</li>
-          <li>Grunt</li>
-          <li>Gulp</li>
-          <li>Webpack</li>
-        </ul>
-        <h2>Operating Systems</h2>
-        <ul>
-          <li>Linux</li>
-          <li>MacOS</li>
-          <li>Windows</li>
-        </ul>
+        <h1>Skills</h1>
+        <div style={{ overflowX: 'auto' }}>
+          {
+            skillz.map(skill =>
+              <div>
+                <h2 style={{marginTop: '2em'}}>{skill.title}</h2>
+                <ul>
+                  {
+                    skill.items.map(item =>
+                      <li>
+                        <Paper style={style} zDepth={3} circle={true} >{item}</Paper>
+                      </li>
+                    )
+                  }
+                </ul>
+              </div>
+            )
+          }
+        </div>
       </div>
-    </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Main = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(<Main />, document.getElementById('root'));
